26632739adf9b44d874a3ccc395348d24c6f3f86
Maxime PHAM-THANH <84508076+MaximePHAMTHANH@users.noreply.github.com>
2022-09-05 12:55:27 +0200

    Add files via upload

--- Blanck.php
+++ Blanck.php
@@ -1,34 +1,32 @@
 <?php
 /*
-Plugin Name: Blanck
+Plugin Name: Convert
 Plugin URI: 
-Description: Blanck
+Description: Convert
 Author: Maxime Pham Thanh
 Author URI: 
 Version: 1.0
 */
 
-if (!defined('ABSPATH') ){
-	die;
-}
+if (!defined('ABSPATH') ){die;}
 
+function Convert($atts, $content=null) { 
 
-function Blanck($atts, $content=null) { 
-
-       wp_register_style('Blanck_namespace', plugins_url('Blanck.css',__FILE__ ));
-       wp_enqueue_style('Blanck_namespace'); 
-       wp_register_script( 'Blanck_namespace_js', plugins_url( 'Blanck.js' , __FILE__ ) );
-       wp_enqueue_script( 'Blanck_namespace_js' );
-       
+       wp_register_style('Convert_namespace', plugins_url('Convert.css',__FILE__ ));
+       wp_enqueue_style('Convert_namespace'); 
+       wp_register_script('Convert_namespace_js', plugins_url( 'Convert.js' , __FILE__ ) );
+       wp_enqueue_script('Convert_namespace_js');
+       wp_register_script('Convert_namespace_js_axios','https://unpkg.com/axios/dist/axios.min.js');
+       wp_enqueue_script('Convert_namespace_js_axios');
 
        $attributes = shortcode_atts( array('id' => 'The Id','type' => 'The Type'), $atts );
 
-
-       echo($attributes['id']." and ".$attributes['type']);
-
-
+       echo('
+		<div class="content">
+			<p>This is a converter</p>
+		</div>
+       	');
 }
 
-
-add_shortcode('Blanck', 'Blanck');  
+add_shortcode('Convert', 'Convert');  
 
